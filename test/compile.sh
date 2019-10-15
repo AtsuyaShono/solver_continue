26ee9519ec236053fb193cf69a0da79c5b0eedbb
Atsuya SHONO <atsuyashono@AtsuyaSHONO-Mac.local>
2019-10-15 20:11:24 +0900

    10/15 20:00

--- test/compile.sh
+++ test/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo g++ -o cada0122 -std=c++11 -Ofast -march=native ../solver-project-final/solver.cpp -Xpreprocessor -fopenmp -lomp
-g++ -o cada0122 -std=c++11 -Ofast -march=native ../solver-project-final/solver.cpp -Xpreprocessor -fopenmp -lomp
+echo g++ -o cada0122 -std=c++11 -Ofast -march=native ../solver-project-final/solver.cpp -fopenmp
+g++ -o cada0122 -std=c++11 -Ofast -march=native ../solver-project-final/solver.cpp -fopenmp
