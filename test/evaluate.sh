f61424cd7d0c2e145d4e93be2e0172726f06e657
Atsuya SHONO <atsuyashono@VLAN5-dhcp146.is.env.kitakyu-u.ac.jp>
2019-10-16 19:15:12 +0900

    10/16 19:30

--- test/evaluate.sh
+++ test/evaluate.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 
-echo "synopsys01.txt
+echo "
+synopsys01.txt
 "
-./evaluaterFast ../testcases/synopsys01.txt outputs/01_out.txt
-echo "synopsys02.txt
+./evaluaterFast testcases/synopsys01.txt outputs/01_out.txt
+echo "
+synopsys02.txt
 "
-./evaluaterFast ../testcases/synopsys02.txt outputs/02_out.txt
-echo "synopsys03.txt
+./evaluaterFast testcases/synopsys02.txt outputs/02_out.txt
+echo "
+synopsys03.txt
 "
-./evaluaterFast ../testcases/synopsys03.txt outputs/03_out.txt
-echo "synopsys04.txt
+./evaluaterFast testcases/synopsys03.txt outputs/03_out.txt
+echo "
+synopsys04.txt
 "
-./evaluaterFast ../testcases/synopsys04.txt outputs/04_out.txt
-echo "synopsys05.txt
+./evaluaterFast testcases/synopsys04.txt outputs/04_out.txt
+echo "
+synopsys05.txt
 "
-./evaluaterFast ../testcases/synopsys05.txt outputs/05_out.txt
-echo "synopsys06.txt
+./evaluaterFast testcases/synopsys05.txt outputs/05_out.txt
+echo "
+synopsys06.txt
 "
-./evaluaterFast ../testcases/synopsys06.txt outputs/06_out.txt
+./evaluaterFast testcases/synopsys06.txt outputs/06_out.txt
