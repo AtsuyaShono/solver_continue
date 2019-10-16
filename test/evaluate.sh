aa69698c67cc9f0c8ce06eb4d95bdc9046a0ef34
Atsuya SHONO <atsuyashono@VLAN5-dhcp146.is.env.kitakyu-u.ac.jp>
2019-10-16 19:07:12 +0900

    10/16 19:10

--- test/evaluate.sh
+++ test/evaluate.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-echo "synopsys01.txt\n"
-./../evluaterFast ../testcases/synopsys01.txt outputs/01_out.txt
-echo "synopsys02.txt\n"
-./../evluaterFast ../testcases/synopsys02.txt outputs/02_out.txt
-echo "synopsys03.txt\n"
-./../evluaterFast ../testcases/synopsys03.txt outputs/03_out.txt
-echo "synopsys04.txt\n"
-./../evluaterFast ../testcases/synopsys04.txt outputs/04_out.txt
-echo "synopsys05.txt\n"
-./../evluaterFast ../testcases/synopsys05.txt outputs/05_out.txt
-echo "synopsys06.txt\n"
-./../evluaterFast ../testcases/synopsys06.txt outputs/06_out.txt
+echo "synopsys01.txt
+"
+./evluaterFast ../testcases/synopsys01.txt outputs/01_out.txt
+echo "synopsys02.txt
+"
+./evluaterFast ../testcases/synopsys02.txt outputs/02_out.txt
+echo "synopsys03.txt
+"
+./evluaterFast ../testcases/synopsys03.txt outputs/03_out.txt
+echo "synopsys04.txt
+"
+./evluaterFast ../testcases/synopsys04.txt outputs/04_out.txt
+echo "synopsys05.txt
+"
+./evluaterFast ../testcases/synopsys05.txt outputs/05_out.txt
+echo "synopsys06.txt
+"
+./evluaterFast ../testcases/synopsys06.txt outputs/06_out.txt
--- test/run.sh
+++ test/run.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 
 date
-echo "synopsys01.txt
+echo "
+synopsys01.txt
 "
 time ./cada0122 ../testcases/synopsys01.txt ../outputs/01_out.txt
 date
-echo "synopsys02.txt
+echo "
+synopsys02.txt
 "
 time ./cada0122 ../testcases/synopsys02.txt ../outputs/02_out.txt
 date
-echo "synopsys03.txt
+echo "
+synopsys03.txt
 "
 time ./cada0122 ../testcases/synopsys03.txt ../outputs/03_out.txt
 date
-echo "synopsys04.txt
+echo "
+synopsys04.txt
 "
 time ./cada0122 ../testcases/synopsys04.txt ../outputs/04_out.txt
 date
-echo "synopsys05.txt
+echo "
+synopsys05.txt
 "
 time ./cada0122 ../testcases/synopsys05.txt ../outputs/05_out.txt
 date
-echo "synopsys06.txt
+echo "
+synopsys06.txt
 "
 time ./cada0122 ../testcases/synopsys06.txt ../outputs/06_out.txt
