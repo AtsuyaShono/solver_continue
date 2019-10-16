878b2779bdc1d6c7c37be23b505690758bf896e5
Atsuya SHONO <atsuyashono@VLAN5-dhcp146.is.env.kitakyu-u.ac.jp>
2019-10-16 19:18:58 +0900

    10/16 19:40

--- test/run.sh
+++ test/run.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 
-date
 echo "
 synopsys01.txt
 "
-time ./cada0122 testcases/synopsys01.txt outputs/01_out.txt
 date
+time ./cada0122 testcases/synopsys01.txt outputs/01_out.txt
 echo "
 synopsys02.txt
 "
-time ./cada0122 testcases/synopsys02.txt outputs/02_out.txt
 date
+time ./cada0122 testcases/synopsys02.txt outputs/02_out.txt
 echo "
 synopsys03.txt
 "
-time ./cada0122 testcases/synopsys03.txt outputs/03_out.txt
 date
+time ./cada0122 testcases/synopsys03.txt outputs/03_out.txt
 echo "
 synopsys04.txt
 "
-time ./cada0122 testcases/synopsys04.txt outputs/04_out.txt
 date
+time ./cada0122 testcases/synopsys04.txt outputs/04_out.txt
 echo "
 synopsys05.txt
 "
-time ./cada0122 testcases/synopsys05.txt outputs/05_out.txt
 date
+time ./cada0122 testcases/synopsys05.txt outputs/05_out.txt
 echo "
 synopsys06.txt
 "
+date
 time ./cada0122 testcases/synopsys06.txt outputs/06_out.txt
